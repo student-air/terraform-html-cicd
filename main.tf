2aa56e45dd9c1a1eb643b4d4a4f35f13f961a543
SAKHI <your_email@example.com>
2025-12-22 13:05:56 +0500

    Updated content

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+  key_name      = "html-cicd-key"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              docker rm -f $(docker ps -aq) || true
+              docker pull student972/html-cicd:latest
+              docker run -d -p 80:80 student972/html-cicd:latest
+              EOF
+
+  tags = {
+    Name = "html-cicd"
+  }
+}
